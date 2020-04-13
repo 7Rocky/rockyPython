efeb0c3884c9cb494211996bf503479bb5eef2ad
Rocky <rockygitt@gmail.com>
2020-04-14 00:50:52 +0200

    Fix

--- src/server.py
+++ src/server.py
@@ -1,5 +1,5 @@
-import bicimad_api.app
-#import hello_app.app
+#import bicimad_api.app
+import hello_app.app
 import os
 
 from flask import Flask
@@ -8,6 +8,6 @@ app = Flask(__name__)
 port = int(os.getenv('PORT', 8000))
 
 if __name__ == '__main__':
-  bicimad_api.app.main(app)
-  #hello_app.app.main(app)
+  #bicimad_api.app.main(app)
+  hello_app.app.main(app)
   app.run(host='0.0.0.0', port=port, debug=True)
